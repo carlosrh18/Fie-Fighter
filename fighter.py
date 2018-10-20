bbc1226295a793ff48ccc58cd31805e6211f2b81
Carlos Andres Robles <34143034+carlosrh18@users.noreply.github.com>
2018-10-20 01:27:29 -0500

    Add files via upload

---
+++ fighter.py
@@ -0,0 +1,41 @@
+# Autor: Ing. Carlos Andres Robles Hernandez
+if __name__ == '__main__':
+    import sys
+    import subprocess
+    import time
+
+    print('Cargando FIE FIGHTER', end='', flush=True)
+    # Check if pygame is installed, if not install pygame
+    # If os is not windows, then tell to manually install
+    if str(sys.platform) == 'win32':
+        try:
+            import pygame
+        except ModuleNotFoundError:
+            print('X')
+            print('Pygame is not installed! Installing...')
+            subprocess.call(['py', '-m', 'pip', 'install', 'pygame'])
+            print('Finished, continuing...')
+            import pygame
+    else:
+        try:
+            import pygame
+        except ModuleNotFoundError:
+            print('YOU ARE USING LINUX/MAC, PLEASE INSTALL PYGAME MANUALLY')
+            time.sleep(3)
+            sys.exit()
+
+    # Import Game Module [Which Loads Sprites, Global, Media, etc.]
+    sys.path.insert(0, './data')
+    import game
+
+    print('Done')
+
+    #pygame.init()
+
+    pygame.init()
+    game.title_screen()
+    game.mode_select()
+    game.char_select()
+    while game.globals.superloop:
+        game.main()
+    pygame.quit()
